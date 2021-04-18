445fd25055eaab2596820bba7c6182cb8d3b859c
apb <prie@itu.dk>
2021-04-18 17:15:34 +0200

    fixed database path

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ INSERT INTO notes VALUES(null,2,"1993-09-23 12:10:10","i want lunch pls",1234567
 
 # APPLICATION SETUP #
 app = Flask(__name__)
-app.database = "db.sqlite3"
+app.database = "db.sqlite3.db"
 app.secret_key = os.urandom(32)
 
 
